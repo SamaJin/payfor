51f6dcebd0d599ce5c7045f85c72265497247d16
lm <lm@DESKTOP-MM702ML>
2016-04-07 10:59:10 +0800

    bean

---
+++ ccdms/src/com/helphand/ccdms/beans/callout_tj_bean.java
@@ -0,0 +1,63 @@
+package com.helphand.ccdms.beans;
+
+/**
+ * 贵州自有铃音
+ */
+public class callout_tj_bean {
+
+	private String nums;
+	private String script_id;
+	private String script_name;
+	private String result_describe;
+	/**
+	 * @return the nums
+	 */
+	public String getNums() {
+		return nums;
+	}
+	/**
+	 * @param nums the nums to set
+	 */
+	public void setNums(String nums) {
+		this.nums = nums;
+	}
+	/**
+	 * @return the script_id
+	 */
+	public String getScript_id() {
+		return script_id;
+	}
+	/**
+	 * @param script_id the script_id to set
+	 */
+	public void setScript_id(String script_id) {
+		this.script_id = script_id;
+	}
+	/**
+	 * @return the script_name
+	 */
+	public String getScript_name() {
+		return script_name;
+	}
+	/**
+	 * @param script_name the script_name to set
+	 */
+	public void setScript_name(String script_name) {
+		this.script_name = script_name;
+	}
+	/**
+	 * @return the result_describe
+	 */
+	public String getResult_describe() {
+		return result_describe;
+	}
+	/**
+	 * @param result_describe the result_describe to set
+	 */
+	public void setResult_describe(String result_describe) {
+		this.result_describe = result_describe;
+	}
+	
+ 
+
+}
